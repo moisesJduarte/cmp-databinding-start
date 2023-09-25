a63a982132db100a29de3ea878233bf1bf543dbc
Moises Duarte <gladmd19@gmail.com>
2023-09-25 16:37:44 -0400

    local references

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,7 @@ export class CockpitComponent implements OnInit {
   @Output() bluePrintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
 
 
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
 
@@ -21,11 +21,27 @@ export class CockpitComponent implements OnInit {
   }
 
 
-  onAddServer() {
-    this.serverCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent })
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent })
   }
 
-  onAddBlueprint() {
-    this.bluePrintCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent })
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.bluePrintCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent })
   }
 }
+
+
+
+// In Angular 8+, the @ViewChild() syntax which you'll see in the next lecture needs to be changed slightly:
+
+// Instead of:
+
+// @ViewChild('serverContentInput') serverContentInput: ElementRef;
+// use
+
+// @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
+// The same change (add { static: true } as a second argument) needs to be applied to ALL usages of @ViewChild() (and also @ContentChild() which you'll learn about later) IF you plan on accessing the selected element inside of ngOnInit().
+
+// If you DON'T access the selected element in ngOnInit (but anywhere else in your component), set static: false instead!
+
+// If you're using Angular 9+, you only need to add { static: true } (if needed) but not { static: false }.
