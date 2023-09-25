656d342fe5d4816d8f9ca43a4a3c80dfd25f3d67
Moises Duarte <gladmd19@gmail.com>
2023-09-25 09:32:37 -0400

    bug fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{ type: 'server', name: 'Testserver', content: 'just a test' }];
+
   onServerAdded(serverData: { serverName: string, serverContent: string }) {
     this.serverElements.push({
       type: 'server',
@@ -15,7 +16,7 @@ export class AppComponent {
     });
   }
 
-  onAddBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
     this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.serverName,
